c26fd4a97a3ba4ee1226d978ed0297f0e155486d
Afshan kanwal22 <kanwalafshan2244@gmail.com>
2026-04-10 23:52:15 +0500

    UPDATE

--- smit-portal/src/App.jsx
+++ smit-portal/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Courses from "./pages/Courses";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
